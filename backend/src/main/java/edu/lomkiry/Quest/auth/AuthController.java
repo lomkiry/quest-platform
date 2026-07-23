efd5f3dc40c50c24a450296bb6fc8aba642a3622
kirill <kirillrabotaxd@gmail.com>
2026-07-23 13:43:27 +0700

    security and auth(register)

--- backend/src/main/java/edu/lomkiry/Quest/auth/AuthController.java
+++ backend/src/main/java/edu/lomkiry/Quest/auth/AuthController.java
@@ -1,10 +1,41 @@
 package edu.lomkiry.Quest.auth;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+@RestController
 @RequestMapping("/api/auth")
 public class AuthController {
+
+    private final AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    public record AuthRequest(
+        @NotBlank
+        String username,
+
+        @Email
+        @NotBlank
+        String email,
+
+        @NotBlank
+        String password
+    ) {}
     
+    @PostMapping("/register")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void registerUser(@Valid @RequestBody AuthRequest request) {
+        authService.registerUser(request.username(), request.email(), request.password());
+    } 
 }
--- backend/src/main/java/edu/lomkiry/Quest/auth/AuthService.java
+++ backend/src/main/java/edu/lomkiry/Quest/auth/AuthService.java
@@ -1,8 +1,27 @@
 package edu.lomkiry.Quest.auth;
 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import edu.lomkiry.Quest.users.User;
+import edu.lomkiry.Quest.users.UserRepository;
 
 @Service
 public class AuthService {
-    
+        private final UserRepository userRepository;
+        private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+        AuthService(UserRepository userRepository, BCryptPasswordEncoder bCryptPasswordEncoder) {
+            this.userRepository = userRepository;
+            this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+        }
+
+        public void registerUser(String username, String email, String password) {
+            User user = new User();
+            user.setEmail(email);
+            user.setUsername(username);
+            user.setPassword(bCryptPasswordEncoder.encode(password));
+
+            userRepository.save(user);
+        }
 }
